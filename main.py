b8683b6c28502b43326e68d4318183f309fb2b03
Кристина Петрова <kristina.s.petrova185@gmail.com>
2026-05-16 01:38:10 +0300

    library system added

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Library project")
+books = []
+
+while True:
+    print("\n1. Добави книга")
+    print("2. Покажи книги")
+    print("3. Изход")
+
+    choice = input("Избери опция: ")
+
+    if choice == "1":
+        title = input("Въведи име на книга: ")
+        books.append(title)
+        print("Книгата е добавена.")
+
+    elif choice == "2":
+        print("Списък с книги:")
+        for book in books:
+            print(book)
+
+    elif choice == "3":
+        break
+
+    else:
+        print("Невалиден избор.")
